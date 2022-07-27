f0323e155d6d3b01e545b2f47515fa6b978c8a1d
zedchamaa <sales@nineprovince.com>
2022-07-27 11:41:43 +0200

    Add conversions from liter to other units

--- volume.js
+++ volume.js
@@ -1,3 +1,5 @@
+//==========CONVERSIONS FROM CUBIC METER==========//
+
 // Trigger the convert from cubic meter functions upon data input
 const cubicMeterElement = document.getElementById("cubic-meter");
 cubicMeterElement.addEventListener('input', (event) => { 
@@ -30,4 +32,40 @@ function convertCubicMeterToMilliliter(cubicMeter) {
   let cubicMeterMilliliter = cubicMeter * 1000000;
   cubicMeterMilliliter = Number(cubicMeterMilliliter.toFixed(2));
   cubicMeterMilliliterElement.value = cubicMeterMilliliter;
+}
+
+//==========CONVERSIONS FROM LITER==========//
+
+// Trigger the convert from liter functions upon data input
+const literElement = document.getElementById("liter");
+literElement.addEventListener('input', (event) => { 
+  const { target } = event;
+  let liter = target.value;
+  convertLiterToCubicMeter(liter);
+  convertLiterToCubicCm(liter);
+  convertLiterToMilliliter(liter);
+});
+
+// Convert from Liter to Cubic meter
+function convertLiterToCubicMeter(liter) {
+  let literCubicMeterElement = document.getElementById("cubic-meter");
+  let literCubicMeter = liter / 1000;
+  literCubicMeter = Number(literCubicMeter.toFixed(10));
+  literCubicMeterElement.value = literCubicMeter;
+}
+
+// Convert Liter to Cubic cm
+function convertLiterToCubicCm(liter) {
+  let literCubicCmElement = document.getElementById("cubic-centimeter");
+  let literCubicCm = liter * 1000;
+  literCubicCm = Number(literCubicCm.toFixed(2));
+  literCubicCmElement.value = literCubicCm;
+}
+
+// Convert Liter to Milliliter
+function convertLiterToMilliliter(liter) {
+  let literMilliliterElement = document.getElementById("milliliter");
+  let literMilliliter = liter * 1000;
+  literMilliliter = Number(literMilliliter.toFixed(2));
+  literMilliliterElement.value = literMilliliter;
 }
