a7df10fa4486b3286b2d8247a3ec1667a5dca719
zedchamaa <sales@nineprovince.com>
2022-08-22 11:09:45 +0200

    Add conversions from hour to other units

--- time.js
+++ time.js
@@ -51,3 +51,57 @@ function convertDayToYear(day) {
   dayYear = Number(dayYear.toFixed(7));
   dayYearElement.value = dayYear;
 }
+
+// ========== CONVERSIONS FROM HOUR TO OTHER UNITS ========== //
+
+// Trigger the convert from Hour functions upon data input
+const hourElement = document.getElementById("hour");
+hourElement.addEventListener('input', (event) => { 
+  const { target } = event;
+  let hour = target.value;
+  convertHourToDay(hour);
+  convertHourToMinute(hour);
+  convertHourToSecond(hour);
+  convertHourToWeek(hour);
+  convertHourToYear(hour);
+});
+
+// Convert Hour to Day
+function convertHourToDay(hour) {
+  const hourDayElement = document.getElementById("day");
+  let hourDay = hour / 24;
+  hourDay = Number(hourDay.toFixed(2));
+  hourDayElement.value = hourDay;
+}
+
+// Convert Hour to Minute
+function convertHourToMinute(hour) {
+  const hourMinuteElement = document.getElementById("minute");
+  let hourMinute = hour * 60;
+  hourMinute = Number(hourMinute.toFixed(2));
+  hourMinuteElement.value = hourMinute;
+}
+
+// Convert Hour to Second
+function convertHourToSecond(hour) {
+  const hourSecondElement = document.getElementById("second");
+  let hourSecond = hour * 3600;
+  hourSecond = Number(hourSecond.toFixed(2));
+  hourSecondElement.value = hourSecond;
+}
+
+// Convert Hour to Week
+function convertHourToWeek(hour) {
+  const hourWeekElement = document.getElementById("week");
+  let hourWeek = hour / 168;
+  hourWeek = Number(hourWeek.toFixed(7));
+  hourWeekElement.value = hourWeek;
+}
+
+// Convert Hour to Year
+function convertHourToYear(hour) {
+  const hourYearElement = document.getElementById("year");
+  let hourYear = hour / 8760;
+  hourYear = Number(hourYear.toFixed(7));
+  hourYearElement.value = hourYear;
+}
