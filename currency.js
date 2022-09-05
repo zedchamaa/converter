111199c875b4fdbe47eae80465b366e7821a7851
Zed Chamaa <sales@nineprovince.com>
2022-09-05 15:52:48 +0200

    Add conversions from usd to other currencies

--- currency.js
+++ currency.js
@@ -28,6 +28,48 @@ async function getLatestRates() {
   const usdCNY = data.rates.CNY;
   const usdSEK = data.rates.SEK;
   const usdNZD = data.rates.NZD;
+
+  // ========== CONVERSIONS FROM USD TO OTHER UNITS ========== //
+
+  // Trigger the convert from USD functions upon data input
+  const usdElement = document.getElementById("usd");
+  usdElement.addEventListener('input', (event) => { 
+  const { target } = event;
+  let usd = target.value;
+
+  // Convert from USD to other currencies
+  function convertFrom(usd) {
+    // Convert USD to EUR
+    convertCurrency(usd, "eur", usdEUR);
+    // Convert USD to JPY
+    convertCurrency(usd, "jpy", usdJPY);
+    // Convert USD to GBP
+    convertCurrency(usd, "gbp", usdGBP);
+    // Convert USD to AUD
+    convertCurrency(usd, "aud", usdAUD);
+    // Convert USD to CAD
+    convertCurrency(usd, "cad", usdCAD);
+    // Convert USD to CHF
+    convertCurrency(usd, "chf", usdCHF);
+    // Convert USD to CNY
+    convertCurrency(usd, "cny", usdCNY);
+    // Convert USD to SEK
+    convertCurrency(usd, "sek", usdSEK);
+    // Convert USD to NZD
+    convertCurrency(usd, "nzd", usdNZD);
+  }
+  
+  convertFrom(usd);
+
+  // Convert from one currency to another
+  function convertCurrency(originCurrency, targetCurrency, exchangeRate) {
+    let conversion = originCurrency * exchangeRate;
+    conversion = Number(conversion.toFixed(2));
+    const element = document.getElementById(targetCurrency);
+    element.value = conversion;
+  }
+
+  });
 }
 
-window.onload = getLatestRates();
+window.onload = getLatestRates(); 
