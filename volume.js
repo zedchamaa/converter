3b1f61f65bf59db62960d7497811cbfff8207f0d
Zed Chamaa <sales@nineprovince.com>
2022-09-11 16:52:53 +0200

    Improve the conversions from Liter to other units

--- volume.js
+++ volume.js
@@ -21,3 +21,27 @@ function convertFromCubicMeter(cubicMeter, targetUnit) {
   conversion = Number(conversion.toFixed(decimalPoint));
   element.value = conversion;
 }
+
+// ========== CONVERSIONS FROM LITER METER TO OTHER UNITS ========== //
+
+// Trigger the convert from Liter functions upon data input
+const literElement = document.getElementById("liter");
+literElement.addEventListener('input', (event) => { 
+  const { target } = event;
+  let liter = target.value;
+  convertFromLiter(liter, 'cubic-meter');
+  convertFromLiter(liter, 'cubic-centimeter');
+  convertFromLiter(liter, 'milliliter');
+});
+
+// Convert from Liter to other units
+function convertFromLiter(liter, targetUnit) {
+  const element = document.getElementById(targetUnit);
+  let conversion;
+  let decimalPoint;
+  if (targetUnit === 'cubic-meter') conversion = liter / 1000, decimalPoint = 10;
+  if (targetUnit === 'cubic-centimeter') conversion = liter * 1000, decimalPoint = 2;
+  if (targetUnit === 'milliliter') conversion = liter * 1000, decimalPoint = 2;
+  conversion = Number(conversion.toFixed(decimalPoint));
+  element.value = conversion;
+}
